daf5dff658a46ae9cfdbe575a82bb6fe19dd3ba1
cyyang <cyyang@g.ncu.edu.tw>
2020-08-04 16:21:51 +0800

    init project

---
+++ test/test_uart.c
@@ -0,0 +1,15 @@
+/**
+ * @file   test_uart.c
+ * @author cy023
+ * @date   2020.08.04
+ * @brief  uart 測試程式
+ */
+
+#include "../src/uart.h"
+
+int main() {
+    UART_init();
+    printf("here is test la.\n");
+    
+    return 0;
+}
